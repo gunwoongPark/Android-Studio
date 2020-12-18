ec1774fa18fe2252727a20612e84dacf0531b34a
20160450 <olympiodoros@naver.com>
2020-12-19 01:25:25 +0900

    add cinematic function

--- app/src/main/java/kr/ac/kumoh/ce/s20160450/lol/ui/notifications/CinematicFragment.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20160450/lol/ui/notifications/CinematicFragment.kt
@@ -1,31 +1,79 @@
 package kr.ac.kumoh.ce.s20160450.lol.ui.notifications
 
+import android.graphics.Bitmap
+import android.media.MediaPlayer
+import android.media.ThumbnailUtils
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.MediaController
 import android.widget.TextView
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kr.ac.kumoh.ce.s20160450.lol.R
 
 class CinematicFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var model: CinematicViewModel
+    private val mAdapter = CinematicAdapter()
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-                ViewModelProvider(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        model = ViewModelProvider(activity as AppCompatActivity).get(CinematicViewModel::class.java)
+
+        model.list.observe(viewLifecycleOwner, Observer<ArrayList<CinematicViewModel.Cinematic>> {
+            mAdapter.notifyDataSetChanged()
         })
+
+        val root = inflater.inflate(R.layout.fragment_cinematic, container, false)
+
+        val lsResult = root.findViewById<RecyclerView>(R.id.lsResult)
+        lsResult.apply {
+            layoutManager = LinearLayoutManager(activity)
+            setHasFixedSize(true)
+            itemAnimator = DefaultItemAnimator()
+            adapter = mAdapter
+        }
+
         return root
+    }
+    
+    inner class CinematicAdapter:RecyclerView.Adapter<CinematicAdapter.ViewHolder>(){
+        
+        inner class ViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView){
+            val title: TextView = itemView.findViewById<TextView>(R.id.title)
+            val path : VideoView = itemView.findViewById<VideoView>(R.id.videoView)
+        }
+
+        override fun getItemCount(): Int {
+            return model.getSize()
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = layoutInflater.inflate(
+                    R.layout.item_cinematic,
+                    parent,
+                    false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.title.text = model.getCinematic(position).title
+            holder.path.setVideoPath(model.getVideoUrl(position))
+            holder.path.setMediaController(MediaController(holder.path.context))
+            holder.path.pause()
+        }
     }
 }
---
+++ app/src/main/java/kr/ac/kumoh/ce/s20160450/lol/ui/notifications/CinematicViewModel.kt
@@ -0,0 +1,92 @@
+package kr.ac.kumoh.ce.s20160450.lol.ui.notifications
+
+import android.app.Application
+import android.graphics.Bitmap
+import android.util.LruCache
+import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.ImageLoader
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONArray
+import org.json.JSONObject
+import java.net.URLEncoder
+
+class CinematicViewModel(application:Application) : AndroidViewModel(application) {
+    companion object{
+        const val QUEUE_TAG = "VolleyRequest"
+        val SERVER_URL="http://192.168.0.11:8080"
+    }
+
+    private var mQueue:RequestQueue
+
+    data class Cinematic(var id:Int, var path:String, var title:String)
+
+    val list = MutableLiveData<ArrayList<Cinematic>>()
+    private val cinematic = ArrayList<Cinematic>()
+
+    val imageLoader:ImageLoader
+    init {
+        list.value = cinematic
+        mQueue = Volley.newRequestQueue(application)
+
+        imageLoader = ImageLoader(mQueue,
+            object:ImageLoader.ImageCache{
+                private val cache = LruCache<String, Bitmap>(100)
+                override fun getBitmap(url: String?): Bitmap {
+                    return cache.get(url)
+                }
+                override fun putBitmap(url: String?, bitmap: Bitmap?) {
+                    cache.put(url,bitmap)
+                }
+            })
+
+        requestCinematic()
+    }
+
+    fun getVideoUrl(i: Int):String = "$SERVER_URL/videos/" + URLEncoder.encode(cinematic[i].path, "utf-8")
+
+    fun getCinematic(i:Int) = cinematic[i]
+
+    fun getSize() = cinematic.size
+
+    fun requestCinematic(){
+        val request = JsonArrayRequest(
+                Request.Method.GET,
+                SERVER_URL + "/cinematic",
+                null,
+                {
+                    cinematic.clear()
+                    parseJson(it)
+                    list.value = cinematic
+                },
+                {
+                    Toast.makeText(getApplication(), it.toString(), Toast.LENGTH_LONG).show()
+                }
+        )
+
+        request.tag = QUEUE_TAG
+        mQueue.add(request)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        mQueue.cancelAll(QUEUE_TAG)
+    }
+
+    private fun parseJson(items: JSONArray) {
+        for (i in 0 until items.length()){
+            val item: JSONObject = items[i] as JSONObject
+            val id = item.getInt("id")
+            val path = item.getString("path")
+            val title = item.getString("title")
+
+            cinematic.add(Cinematic(id,path,title))
+        }
+    }
+}
